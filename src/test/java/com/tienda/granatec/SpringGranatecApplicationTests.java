b0a967892e9fa82d1dd4b6f708053b418f38c0e7
diemaes95 <granudo18@gmail.com>
2022-05-10 19:38:00 +0200

    Primer commit archivos añadidos

---
+++ src/test/java/com/tienda/granatec/SpringGranatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tienda.granatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGranatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
