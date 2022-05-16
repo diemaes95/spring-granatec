3b0993a44f5d0b786e34dc8be2555e604dd55d35
diemaes95 <granudo18@gmail.com>
2022-05-16 22:19:07 +0200

    crear el repository y las clases de servidcio, y el metodo show de la clase ProductoController

---
+++ src/main/java/com/tienda/granatec/repository/ProductoRepository.java
@@ -0,0 +1,17 @@
+package com.tienda.granatec.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tienda.granatec.model.Producto;
+
+/*interface para poder obtener todos los
+ *  metodos crud y poder aplicar a producto */
+
+/*JpaRepository<Producto, Integer>---> indicamos la clase que hara referencia
+ * 				 y el tipo de dato que se le aplicara al crud ejemplo por ID  */
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Integer> {
+	
+}
---
+++ src/main/java/com/tienda/granatec/service/ProductoService.java
@@ -0,0 +1,19 @@
+package com.tienda.granatec.service;
+
+import java.util.Optional;
+
+import com.tienda.granatec.model.Producto;
+
+public interface ProductoService {
+	//guardar
+	public Producto save(Producto producto);
+	//Optional nos da la capacidad de valiar si el objeto que mandamos a la base de datos existe o no
+	//comprobar
+	public Optional<Producto> get(Integer id);
+	
+	//actualizar
+	public void update(Producto producto );
+	
+	//borrar
+	public void delete(Integer id);
+}
