f5f7c4558d456ef866d63657f5bdb505bc84f687
diemaes95 <granudo18@gmail.com>
2022-05-17 20:12:00 +0200

    servicio editar añadido , tanto back como front

--- src/main/java/com/tienda/granatec/controller/ProductoController.java
+++ src/main/java/com/tienda/granatec/controller/ProductoController.java
@@ -1,10 +1,13 @@
 package com.tienda.granatec.controller;
 
+import java.util.Optional;
+
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,29 +18,44 @@ import com.tienda.granatec.service.ProductoService;
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
-	
+
 	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
-	
+
 	@Autowired
 	private ProductoService productoService;
-	
+
 	@GetMapping("")
 	public String show(Model model) {
-		model.addAttribute("productos", productoService.findAll());
+		model.addAttribute("productos", productoService.findAll());//lleva datos desde el backend a frontend
 		return "productos/show";
 	}
-	
+
 	@GetMapping("/create")
 	public String create() {
 		return "productos/create";
 	}
+
 	@PostMapping("/save")
 	public String save(Producto producto) {
-		LOGGER.info("Este es el objeto producto {}",producto);
-		Usuario usuario = new Usuario(1,"","","","","","","");
+		LOGGER.info("Este es el objeto producto {}", producto);
+		Usuario usuario = new Usuario(1, "", "", "", "", "", "", "");
 		producto.setUsuario(usuario);
 		productoService.save(producto);
 		return "redirect:/productos";
 	}
-	
+
+	@GetMapping("/edit/{id}")
+	public String edit(@PathVariable Integer id, Model model) {
+		Producto producto = new Producto();
+		Optional<Producto> optionalProducto = productoService.get(id);//con Optional comprueba is es null
+		producto = optionalProducto.get();//le pasamos a producto el id de optional producto
+		LOGGER.info("Producto buscado: {}", producto);
+		model.addAttribute("producto", producto); //lleva datos desde el backend a frontend
+		return "productos/edit";
+	}
+	@PostMapping("/update")
+	public String update(Producto producto) {
+		productoService.update(producto);
+		return "redirect:/productos";
+	}
 }
