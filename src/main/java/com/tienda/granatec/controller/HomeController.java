4e98e321282538e13968e0a1b6d1f5bb8d80c07e
diemaes95 <granudo18@gmail.com>
2022-05-22 10:30:21 +0200

    vista individual de producto añadida en HomeController y html añadido

--- src/main/java/com/tienda/granatec/controller/HomeController.java
+++ src/main/java/com/tienda/granatec/controller/HomeController.java
@@ -1,9 +1,12 @@
 package com.tienda.granatec.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tienda.granatec.service.ProductoService;
@@ -12,6 +15,9 @@ import com.tienda.granatec.service.ProductoService;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
+	
 	@Autowired
 	private ProductoService productoService;//nos permitira obntener los productos
 	
@@ -19,6 +25,12 @@ public class HomeController {
 	public String home(Model model) {
 		model.addAttribute("productos", productoService.findAll());//aqui trae todos los productos con findAll()
 		return "usuario/home";
+	}
+	
+	@GetMapping("productoHome/{id}")
+	public String productoHome(@PathVariable Integer id, Model model ) {
+		LOGGER.info("id producto enviado como parametro {}",id);
+		return "usuario/productoHome";
 	}
 	
 }
