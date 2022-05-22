831c9bf716c37aaa11b053093e333ae0f697e604
diemaes95 <granudo18@gmail.com>
2022-05-22 10:53:33 +0200

    volcado de datos en la vista individual de producto

--- src/main/java/com/tienda/granatec/controller/HomeController.java
+++ src/main/java/com/tienda/granatec/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.tienda.granatec.controller;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,28 +11,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tienda.granatec.model.Producto;
 import com.tienda.granatec.service.ProductoService;
 
 /*Esta clase tendra la logica para mostrar los productos al usuario*/
 @Controller
 @RequestMapping("/")
 public class HomeController {
-	
+
 	private final Logger LOGGER = LoggerFactory.getLogger(HomeController.class);
-	
+
 	@Autowired
-	private ProductoService productoService;//nos permitira obntener los productos
-	
+	private ProductoService productoService;// nos permitira obntener los productos
+
 	@GetMapping("")
 	public String home(Model model) {
-		model.addAttribute("productos", productoService.findAll());//aqui trae todos los productos con findAll()
+		model.addAttribute("productos", productoService.findAll());// aqui trae todos los productos con findAll()
 		return "usuario/home";
 	}
-	
+
 	@GetMapping("productoHome/{id}")
-	public String productoHome(@PathVariable Integer id, Model model ) {
-		LOGGER.info("id producto enviado como parametro {}",id);
+	public String productoHome(@PathVariable Integer id, Model model) {
+		LOGGER.info("id producto enviado como parametro {}", id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id); //Nos devuelve el producto segun su id 
+		producto = productoOptional.get();
+		model.addAttribute("producto", producto);
 		return "usuario/productoHome";
 	}
-	
+
 }
