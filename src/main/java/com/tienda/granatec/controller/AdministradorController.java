3d8a108a5b264e955b5fe9a40cbc5deea977434f
diemaes95 <granudo18@gmail.com>
2022-05-31 21:08:07 +0200

    funcionalidad para ver usuarios admin

--- src/main/java/com/tienda/granatec/controller/AdministradorController.java
+++ src/main/java/com/tienda/granatec/controller/AdministradorController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.tienda.granatec.model.Producto;
 import com.tienda.granatec.service.IProductoService;
+import com.tienda.granatec.service.IUsuarioService;
 
 @Controller
 @RequestMapping("/administrador")
@@ -17,11 +18,20 @@ public class AdministradorController {
 	@Autowired
 	private IProductoService productoService;
 	
+	@Autowired
+	private IUsuarioService usuarioService;
+	
 	@GetMapping("")
 	public String home(Model model) {//cuando se haga la peticion al metodo devuelve los productos
 		List<Producto> productos = productoService.findAll();
 		model.addAttribute("productos", productos);
 		return "administrador/home";
 		
+	}
+	
+	@GetMapping("/usuarios")
+	public String usuarios(Model model) {
+		model.addAttribute("usuarios", usuarioService.findAll());
+		return "administrador/usuarios";
 	}
 }
--- src/main/java/com/tienda/granatec/service/IUsuarioService.java
+++ src/main/java/com/tienda/granatec/service/IUsuarioService.java
@@ -1,10 +1,12 @@
 package com.tienda.granatec.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.tienda.granatec.model.Usuario;
 
 public interface IUsuarioService {
+	List<Usuario> findAll();
 	Optional <Usuario> findById(Integer Id);
 	Usuario save (Usuario usuario);
 	Optional<Usuario> findByEmail(String email);
